7b3fa91f614cb3b3189faa8dabb22f57dc9dbd38
RBELSON <rbbelson@yahoo.com>
2018-09-17 15:35:27 -0500

    update cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,75 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Coursera Assignment - R Programming - Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+# These 2 functions are useful because they allow the user to calculate the inverse of their matrix and cache the results.
+# This can help avoid recomputing the same inverse matrix repeatedly and allow the user to quickly access the stored result.
+
+# This first function, makeCacheMatrix() helps facilitate the caching by: 
+#           setting the value of the matrix, getting the value of the matrix, 
+#           setting the value of the inverse, and getting the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_mat <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_mat <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv_mat <<- inverse
+    getinverse <- function() inv_mat
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+# This 2nd function cacheSolve(), returns the inverse of the matrix from the cache if it has already been calculated.  If not,
+#  then it will calculate the inverse and set the value in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv_mat <- x$getinverse()
+    if(!is.null(inv_mat)) {
+        message("getting cached matrix")
+        return(inv_mat)
+    }
+    data <- x$get()
+    inv_mat <- solve(data, ...)
+    x$setinverse(inv_mat)
+    inv_mat
 }
+
+
+###################################################################################################
+#  Testing to see if new functions work
+
+test_matrix <- matrix(c(1:4), 2, 2)
+
+# Quick view of test_matrix:
+
+#        [,1] [,2]
+#   [1,]   1    3
+#   [2,]   2    4
+
+
+test_cachematrix <- makeCacheMatrix(test_matrix)
+
+cacheSolve(test_cachematrix)
+
+# Quick view of Output of cacheSolve(test_cachematrix) -- No cache in first run
+
+#        [,1] [,2]
+#   [1,]  -2   1.5
+#   [2,]   1  -0.5
+
+
+# Test again -- see if it is retrieved from cache
+
+cacheSolve(test_cachematrix)
+
+# getting cached matrix
+
+#        [,1] [,2]
+#   [1,]  -2   1.5
+#   [2,]   1  -0.5
+
+#  Success!
